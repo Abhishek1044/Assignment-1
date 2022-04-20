61a7a538d344833cbeb1918489417c931949831f
ABHISHEK TOMAR <103330372+Abhishek1044@users.noreply.github.com>
2022-04-20 21:09:19 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -1,5 +1,5 @@
 gross=int(input("Enter your Gross Income in U.S. $ : \n \t"))
 dep=int(input("Enter number of dependents : \n \t "))
 taxable_income= ( (gross - 10000) - dep*3000 )
-Tax= taxable_income*(0.2)
+Tax= taxable_income*(0.2)   # as tax rate is 20 % i.e. 20/100 which is 0.2 as factor for multiplication
 print(f"Your Tax is : {Tax}")
